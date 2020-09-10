02fddfae210ac875f83a368a9b536fcbdfd6ffe4
Sasanka Reddy Chintam <schintam@virtusa.com>
2020-09-10 18:17:27 -0400

    Code Blocks and expressions

---
+++ new-java-course/src/KeywordsAndExpressions.java
@@ -0,0 +1,21 @@
+public class KeywordsAndExpressions {
+
+    public static void main(String[] args) {
+        //53 reserved words in java
+
+        double kilometers = (100d * 1.69344d);
+        System.out.println(kilometers);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("High Score");
+        }
+
+        int myVariable = 50; //complete line is statement
+        //myVariable = 50 is expression
+
+        myVariable++; //statement
+        System.out.println("Test something");//full line is statement
+
+
+    }
+}
