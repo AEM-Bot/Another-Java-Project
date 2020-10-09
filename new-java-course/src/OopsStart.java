2f6351b68c127ef9f38915fa05bddb3a29805123
Sasanka Reddy Chintam <c-sasanka.reddy@charter.com>
2020-10-09 17:01:21 -0400

    OOPS Concepts Basics

---
+++ new-java-course/src/OopsStart.java
@@ -0,0 +1,12 @@
+public class OopsStart {
+
+    //class --> template or blueprint
+    /*
+        Benefits of classes? cons of primitive data type
+        overcome the limitations of predefined in java
+     */
+    //objects --> state and behavior
+    public static void main(String[] args) {
+
+    }
+}
